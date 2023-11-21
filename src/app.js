ae429eef5ca6f3ffb48aeb1751c987aff03986ca
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2023-11-21 21:49:33 +0000

    with.com

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let pronoun = ["the", "our", "My", "Your"];
+
+  let adj = ["bad", "small", "Skinny", "Told"];
+
+  let noun = ["dog", "dinosaur", "child", "Tree"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++)
+        console.log(pronoun[i] + adj[a] + noun[n] + ".com");
+    }
+  }
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
