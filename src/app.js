6dda176e80061e4c7cff21dd5bdca3823d7ede7f
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2023-11-21 21:58:35 +0000

    withOthersExtensions

--- src/app.js
+++ src/app.js
@@ -6,17 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our", "My", "Your"];
+  let pronoun = ['the', 'our', 'My', 'Your'];
 
-  let adj = ["bad", "small", "Skinny", "Told"];
+  let adj = ['bad', 'small', 'Skinny', 'Told'];
 
-  let noun = ["dog", "dinosaur", "child", "Tree"];
+  let noun = ['dog', 'dinosaur', 'child', 'Tree'];
+
+  let extension = ['.com', '.es', '.let', '.net'];
+
 
   for (let i = 0; i < pronoun.length; i++) {
-    for (let a = 0; a < adj.length; a++) {
-      for (let n = 0; n < noun.length; n++)
-        console.log(pronoun[i] + adj[a] + noun[n] + ".com");
-    }
+  for (let a = 0; a < adj.length; a++)  {
+  for (let n = 0; n < noun.length; n++)  {
+  for (let e = 0; e < extension.length; e++)     
+  console.log(pronoun[i] + adj[a] + noun[n] + extension[e]);
+  }
+  }
   }
   //write your code here
 };
